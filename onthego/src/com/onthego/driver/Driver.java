b6bdb7c442df0e9f602e9f290f1fed8983a222a1
developingiam <developingiam@gmail.com>
2016-03-29 22:54:06 +0530

    created OTGUser and defined the bean for the same in spring configuration

--- onthego/src/com/onthego/driver/Driver.java
+++ onthego/src/com/onthego/driver/Driver.java
@@ -1,9 +1,17 @@
 package com.onthego.driver;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.onthego.beans.OTGUser;
+
 public class Driver {
 
 	public static void main(String[] args) {
-		System.out.println("Application started - "+new java.util.Date());
+		ApplicationContext factory = new ClassPathXmlApplicationContext("spring-config.xml");
+		OTGUser user = (OTGUser)factory.getBean("otgUser");
+		System.out.println(user.getId()+"\n"+user.getFirstName()+"\n"+user.getMiddleName()+"\n"+user.getLastName());
+		
 	}
 
 }
